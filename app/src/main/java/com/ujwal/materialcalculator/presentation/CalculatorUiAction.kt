1bb71a5aa1db0ce1c0eda4790bb36f49bb6d90e3
Ujwal Garg <ujwalgarg2001@gmail.com>
2025-02-18 11:21:03 +0530

    -Migrated Material2 to Material3 -Added CalculatorUiActions.kt

---
+++ app/src/main/java/com/ujwal/materialcalculator/presentation/CalculatorUiAction.kt
@@ -0,0 +1,18 @@
+package com.ujwal.materialcalculator.presentation
+
+import androidx.compose.runtime.Composable
+import com.ujwal.materialcalculator.domain.CalculatorAction
+
+data class CalculatorUiAction(
+    val text: String?,
+    val highlightLevel: HighlightLevel,
+    val action: CalculatorAction,
+    val content: @Composable () -> Unit = {}
+)
+
+sealed interface HighlightLevel {
+    data object Neutral : HighlightLevel
+    data object SemiHighlighted : HighlightLevel
+    data object Highlighted : HighlightLevel
+    data object StronglyHighlighted : HighlightLevel
+}
